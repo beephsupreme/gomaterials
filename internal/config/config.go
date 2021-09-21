b2255f88e854185b6ce124688e22943175aa8d4f
Michael Rowsey <beephsupreme@gmail.com>
2021-09-21 10:21:01 -0700

    update AppConfig struct for json loading

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,27 +1,27 @@
 package config
 
 type AppConfig struct {
-	dataPath        string
-	backlog         string
-	data            string
-	hfr             string
-	outfile         string
-	translate       string
-	validate        string
-	scheduleURL     string
-	header          string
-	firstLineText   string
-	firstLineNumber int
-	factor          int
-	bits            int
-	scheduleWidth   int
-	numDates        int
-	pn              int
-	oh              int
-	oo              int
-	ro              int
-	qty             int
-	tli             int
-	toki            int
-	uom             int
+	DataPath        string `json:"DataPath"`
+	Backlog         string `json:"Backlog"`
+	Data            string `json:"Data"`
+	Hfr             string `json:"Hfr"`
+	Outfile         string `json:"Outfile"`
+	Translate       string `json:"Translate"`
+	Validate        string `json:"Validate"`
+	ScheduleURL     string `json:"ScheduleURL"`
+	Header          string `json:"Header"`
+	FirstLineText   string `json:"FirstLineText"`
+	FirstLineNumber int
+	Factor          int `json:"Factor"`
+	Bits            int `json:"Bits"`
+	ScheduleWidth   int `json:"ScheduleWidth"`
+	NumDates        int
+	PN              int `json:"PN"`
+	OH              int `json:"OH"`
+	OO              int `json:"OO"`
+	RO              int `json:"RO"`
+	QTY             int `json:"QTY"`
+	TLI             int `json:"TLI"`
+	TOKI            int `json:"TOKI"`
+	UOM             int `json:"UOM"`
 }
