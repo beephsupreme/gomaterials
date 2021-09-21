0e72148a4fc4d5714c021b9c3507baba0cdb0965
beephsupreme <beephsupreme@gmail.com>
2021-09-20 23:12:36 -0700

    add config.go for AppConfig

---
+++ internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+type AppConfig struct {
+	dataPath        string
+	backlog         string
+	data            string
+	hfr             string
+	outfile         string
+	translate       string
+	validate        string
+	scheduleURL     string
+	header          string
+	firstLineText   string
+	firstLineNumber int
+	factor          int
+	bits            int
+	scheduleWidth   int
+	numDates        int
+	pn              int
+	oh              int
+	oo              int
+	ro              int
+	qty             int
+	tli             int
+	toki            int
+	uom             int
+}
